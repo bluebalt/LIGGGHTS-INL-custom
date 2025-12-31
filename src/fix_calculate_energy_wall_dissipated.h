3a8acf05f3a6dbc8e34b2597acd97ab23e875aca
bluebalt <sew3560@gmail.com>
2025-12-31 18:18:42 +0900

    no message

--- src/fix_calculate_energy_wall_dissipated.h
+++
@@ -1,2 +0,0 @@
-#include "fix.h" 
- namespace LAMMPS_NS {class FixCalculateWallEnergyDissipated : public Fix { public: void dissipate_energy(const double, const bool, const int iPart = -1) {} ; void dissipate_energy_tangential(const double, const bool, const int iPart = -1) {} ; };}
--- src/update_wall_clock_time.h
+++
@@ -1,2 +0,0 @@
-void Update::set_wall_clock_time(int narg, char **arg) { error->all(FLERR,"this feature is not available in your version"); } 
- bool Update::run_into_wall_clock() {return false; }
